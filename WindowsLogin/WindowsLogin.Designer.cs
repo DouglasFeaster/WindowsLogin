9cfd3c654606fd0984203df2ad6798a22609b4e2
DouglasFeaster <douglasgfeaster@gmail.com>
2017-07-12 11:46:56 -0400

    Init

---
+++ WindowsLogin/WindowsLogin.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace WindowsLogin
+{
+    partial class frmWindowsLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.mtbPassword = new System.Windows.Forms.MaskedTextBox();
+            this.cbxDomain = new System.Windows.Forms.ComboBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Location = new System.Drawing.Point(84, 12);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(135, 20);
+            this.tbUsername.TabIndex = 0;
+            // 
+            // mtbPassword
+            // 
+            this.mtbPassword.Location = new System.Drawing.Point(84, 48);
+            this.mtbPassword.Name = "mtbPassword";
+            this.mtbPassword.PasswordChar = '*';
+            this.mtbPassword.Size = new System.Drawing.Size(135, 20);
+            this.mtbPassword.TabIndex = 1;
+            // 
+            // cbxDomain
+            // 
+            this.cbxDomain.FormattingEnabled = true;
+            this.cbxDomain.Location = new System.Drawing.Point(84, 82);
+            this.cbxDomain.Name = "cbxDomain";
+            this.cbxDomain.Size = new System.Drawing.Size(135, 21);
+            this.cbxDomain.TabIndex = 2;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(111, 114);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Domain:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Password:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Username:";
+            // 
+            // frmWindowsLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 149);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.cbxDomain);
+            this.Controls.Add(this.mtbPassword);
+            this.Controls.Add(this.tbUsername);
+            this.MaximizeBox = false;
+            this.Name = "frmWindowsLogin";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.frmWindowsLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.MaskedTextBox mtbPassword;
+        private System.Windows.Forms.ComboBox cbxDomain;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsLogin/WindowsLogin.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Windows.Forms;
+using System.DirectoryServices.ActiveDirectory;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+
+namespace WindowsLogin
+{
+    public partial class frmWindowsLogin : Form
+    {
+        // Native Windows Library for user accounts
+        [DllImport("advapi32.dll")]
+        public static extern bool LogonUser(string userName, string domainName, string password, int LogonType, int LogonProvider, ref IntPtr phToken);
+
+        public frmWindowsLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void frmWindowsLogin_Load(object sender, EventArgs e)
+        {
+            // Gets current domain and sends it to the Combobox in Uppercase
+            cbxDomain.Items.Add(GetDomain().ToUpper());
+
+            // Makes sure a domian is selected by default
+            cbxDomain.SelectedIndex = 0;
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            bool issuccess = false;
+            string username = GetloggedinUserName();
+
+            // If username of current Windows user does not match the username of the person logging into the application 
+            // and if the current Windows user does not belong to the correct security group then the login will fail.
+            if (username.ToLowerInvariant().Contains(tbUsername.Text.Trim().ToLowerInvariant()) && username.ToLowerInvariant().Contains(cbxDomain.Text.Trim().ToLowerInvariant()) && SecGroupMember() == true)
+            {
+                // The given credentials will be validated using the Native Windows Library for user accounts and return a True or False
+                issuccess = IsValidateCredentials(tbUsername.Text.Trim(), mtbPassword.Text.Trim(), cbxDomain.Text.Trim());
+
+                // If the Native Windows Library returns a True then the program will open the specified main form
+                if (issuccess)
+                {
+                    // Insert Form here \/
+                    MessageBox.Show("Success!");
+                    /*
+                      // Creates new Form object
+                      Form form = new frmform();
+                      // Hides Login Form
+                      this.Hide();
+                      // Opens created Form object
+                      frmform.Show();
+                    */
+                }
+            }
+            
+            else
+            {
+                // If the given credentials are not valid, meaning False, then the error message with appear
+                MessageBox.Show("User Name or Password is invalid!", "Login Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        // Retreives the username of the current Windows user and returns it
+        private string GetloggedinUserName()
+        {
+            // Current user
+            WindowsIdentity currentUser = WindowsIdentity.GetCurrent();
+            // Separates and returns current Username
+            return currentUser.Name;
+        }
+
+        // Runs the given credentials through the Native Windows Library and return a True or False
+        private bool IsValidateCredentials(string userName, string password, string domain)
+        {
+            // User security token
+            IntPtr tokenHandler = IntPtr.Zero;
+            // Given credentials run through Native Windows Library and returns True or False
+            bool isValid = LogonUser(userName, domain, password, 2, 0, ref tokenHandler);
+            // Returns True or False value
+            return isValid;
+        }
+
+        // Checks that the current Windows user is a member of the JU_Applications Security group
+        private bool SecGroupMember()
+        {
+            // Current user
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            // Current user's group information
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+            // Confirms membership
+            return principal.IsInRole("Security Group Here");
+        }
+
+        // Gets and returns current Window Domain name
+        private string GetDomain()
+        {
+            // Gets Window Domain name
+            string domain = Domain.GetComputerDomain().ToString();
+
+            // Domain is @.local e.g. campus.local
+            if (domain.Contains(".local"))
+            {
+                // .local is then removed from domain string
+                domain = domain.Replace(".local", "");
+            }
+
+            // domain name is returned
+            return domain;
+        }
+    }
+}
